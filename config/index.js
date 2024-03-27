3fa7f872fa06609717edaabb015bb5fbb372a0ee
SimplyUser7508 <kolya7508@gmail.com>
2024-03-27 22:53:10 +0300

    docs update

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const config = {
     db: {
         mysql : {
-            host: 'localhost',
-            user: 'user', // замените на своего пользователя
-            database: 'appdb', // можете заменить 'appdb' на свое название базы данных
-            password: 'yourPasswordHere', // замените это на пароль от своего пользователя
+                host: 'localhost',
+            user: 'user3', // замените на своего пользователя
+            database: 'db3', // можете заменить 'appdb' на свое название базы данных
+            password: 'AVNS_ThdZaAG2_shkrS8PnP6', // замените это на пароль от своего пользователя
             port: 25060, // порт базы данных
             ssl: {
                 ca: fs.readFileSync('./config/ca-certificate-test.crt'), // Путь к файлу ca.crt
