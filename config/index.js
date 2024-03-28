cf497af53bff7cd8c08f6b8aec99c2137ad094c6
SimplyUser7508 <kolya7508@gmail.com>
2024-03-28 10:41:32 +0300

    update host

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const config = {
     db: {
         mysql : {
-                host: 'localhost',
+                host: 'db-mysql-fra1-51752-do-user-9208055-0.c.db.ondigitalocean.com',
             user: 'user3', // замените на своего пользователя
             database: 'db3', // можете заменить 'appdb' на свое название базы данных
             password: 'AVNS_ThdZaAG2_shkrS8PnP6', // замените это на пароль от своего пользователя
